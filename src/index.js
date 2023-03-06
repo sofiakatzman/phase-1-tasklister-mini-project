2e30a205c979e835baea3893891ca9c87ea7deda
Sofia Katzman <katzmansof@gmail.com>
2023-03-05 19:05:18 -0500

    finished task lister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+
+
+const newTaskDescription = document.getElementById("new-task-description");
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(newTaskDescription.value)
+    form.reset()
+  })
 });
+
+function buildToDo (todo) {
+  let p = document.createElement('ul')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  p.textContent = `${todo} `
+  btn.textContent = `x`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+  }
+
+
+ function handleDelete(e) {
+  e.target.parentNode.remove()
+ } 
